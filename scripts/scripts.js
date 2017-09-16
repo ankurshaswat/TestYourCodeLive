b4a2a9821d5b3c714f2a56e541f4621c484f2c6f
ankurshaswat <ankurshaswat@yahoo.co.in>
2017-09-16 12:03:41 +0530

    initial commit

---
+++ scripts/scripts.js
@@ -0,0 +1,19 @@
+﻿var windowheight = $(window).height();
+var t2 = $("#topbar").height();
+var h=windowheight-t2;
+$(".codecontainer").height(h+"px");
+$(".toggle").click(function(){
+    $(this).toggleClass("selected");
+    var id = $(this).attr("id")+"c";
+    $("#"+id).toggle();
+    var showingdiv=$(".codecontainer").filter(function(){
+      return($(this).css("display")!="none");
+    }).length;
+    var len=100/showingdiv;
+    $(".codecontainer").width(len+"%");
+
+});
+$("#runbutton").click(function(){
+    $("iframe").contents().find("html").html('<style>'+$("#csscode").val()+'</style>'+$("#htmlcode").val());
+    
+});
